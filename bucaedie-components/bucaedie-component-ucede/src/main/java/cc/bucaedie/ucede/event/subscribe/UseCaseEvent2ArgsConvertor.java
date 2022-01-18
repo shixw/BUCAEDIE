9a07b12a700b81d8139986c861bd5de5464dc436
shixianwei2 <shixianwei@jd.com>
2022-01-18 09:11:42 +0800

    增加事件转换中data解析的公共方法

--- bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/event/subscribe/UseCaseEvent2ArgsConvertor.java
+++ bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/event/subscribe/UseCaseEvent2ArgsConvertor.java
@@ -1,6 +1,7 @@
 package cc.bucaedie.ucede.event.subscribe;
 
 import cc.bucaedie.ucede.event.UseCaseEvent;
+import com.alibaba.fastjson.JSON;
 
 import java.util.Map;
 
@@ -15,4 +16,21 @@ public interface UseCaseEvent2ArgsConvertor {
      * @return
      */
     Object[] convert(UseCaseEvent event, Map<String,String> parameter);
+
+
+    /**
+     * 转换时间中data工具方法
+     * @param event
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    default <T> T parseData(UseCaseEvent event,Class<T> clazz) {
+        String dataStr = event.getData();
+        if (dataStr!=null && !"".equals(dataStr)){
+            return JSON.parseObject(dataStr, clazz);
+        }else{
+            throw new IllegalArgumentException("服务单相关事件转换失败，未返回操作情况数据");
+        }
+    }
 }
