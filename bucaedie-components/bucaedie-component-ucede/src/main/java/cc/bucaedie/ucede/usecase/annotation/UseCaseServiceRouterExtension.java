c770fbcdf73e76d98070015dccbf389ffe6edaad
Ⅱ又贝人韦 <shixw_usr@126.com>
2022-07-03 11:27:08 +0800

    修改注释

--- bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/usecase/annotation/UseCaseServiceRouterExtension.java
+++ bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/usecase/annotation/UseCaseServiceRouterExtension.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.lang.annotation.*;
 
 /**
- * 业务用例服务路由注解， 使用次注解标注的实现在spring依赖注入时会优先注入，
+ * 业务用例服务路由注解， 由于标注了 @Primary , 所以在使用此注解标注的实现在spring依赖注入时会优先注入，
  * 避免一个业务用例有多个实现时，spring依赖注入报错
  */
 @Inherited
