b4408619b5f667a5ca489a6c5442367629cd7aa7
Ⅱ又贝人韦 <shixw_usr@126.com>
2025-01-20 14:23:37 +0800

    分发事件逻辑调整

--- bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/event/subscribe/UseCaseEventSubscriberDispatcher.java
+++ bucaedie-components/bucaedie-component-ucede/src/main/java/cc/bucaedie/ucede/event/subscribe/UseCaseEventSubscriberDispatcher.java
@@ -51,22 +51,27 @@ public class UseCaseEventSubscriberDispatcher implements ApplicationContextAware
                         // 粗出需要判断retry
                     }catch (Exception e){
                         log.error("业务事件分发执行此业务事件失败,发生系统异常,业务单号:{},业务事件:{},异常信息：",event.getBizNo(),event.getEventUniqueKey(),e);
-                        // 处理异常，记录事件日志，不要将异常抛出给监听,同时不会影响触发后续用例的执行
-                        UseCaseEvent useCaseEvent = new UseCaseEvent();
-                        useCaseEvent.setEvent("DISPATCHER_CONVERT_FAIL");
-                        useCaseEvent.setUuid(UUIDUtils.getUUID());
-                        useCaseEvent.setBizNo(event.getBizNo());
-                        useCaseEvent.setIdentity(event.getIdentity());
-                        useCaseEvent.setOperator("EVENT:DISPATCHER");
-                        useCaseEvent.setOperationTime(new Date());
-                        useCaseEvent.setMessage("业务事件:"+event.getEventUniqueKey()+"触发执行此业务用例失败,发生系统异常,位置主要为事件转换入参、获取业务身份及扩展等位置,此错误请勿使用业务用例重置功能,具体查看日志,异常信息："+e.getMessage());
-                        useCaseEvent.setUseCaseCode(useCaseEventSubscriber.getTriggerUseCase());
-                        useCaseEvent.setUseCaseServiceCode(useCaseEventSubscriber.getTriggerUseCaseService());
-                        useCaseEvent.setDomain(useCaseEventSubscriber.getTriggerUseCaseDomain());
-                        useCaseEvent.setEventTime(new Date());
-                        useCaseEvent.setTriggerUuid(event.getTriggerUuid());
-                        // 推送相关事件
-                        useCaseEventPublisherManger.publishEvent(useCaseEvent);
+                        try {
+                            // 处理异常，记录事件日志，不要将异常抛出给监听,同时不会影响触发后续用例的执行
+                            UseCaseEvent useCaseEvent = event.getClass().getDeclaredConstructor().newInstance();
+                            useCaseEvent.setEvent("DISPATCHER_CONVERT_FAIL");
+                            useCaseEvent.setUuid(UUIDUtils.getUUID());
+                            useCaseEvent.setBizNo(event.getBizNo());
+                            useCaseEvent.setIdentity(event.getIdentity());
+                            useCaseEvent.setOperator("EVENT:DISPATCHER");
+                            useCaseEvent.setOperationTime(new Date());
+                            useCaseEvent.setMessage("业务事件:"+event.getEventUniqueKey()+"触发执行此业务用例失败,发生系统异常,位置主要为事件转换入参、获取业务身份及扩展等位置,此错误请勿使用业务用例重置功能,具体查看日志,异常信息："+e.getMessage());
+                            useCaseEvent.setUseCaseCode(useCaseEventSubscriber.getTriggerUseCase());
+                            useCaseEvent.setUseCaseServiceCode(useCaseEventSubscriber.getTriggerUseCaseService());
+                            useCaseEvent.setDomain(useCaseEventSubscriber.getTriggerUseCaseDomain());
+                            useCaseEvent.setEventTime(new Date());
+                            useCaseEvent.setTriggerUuid(event.getTriggerUuid());
+                            // 推送相关事件
+                            useCaseEventPublisherManger.publishEvent(useCaseEvent);
+                        } catch (Exception ex) {
+                            log.error("业务事件分发执行此业务事件失败,发生系统异常,推送异常事件失败,业务单号:{},业务事件:{},异常信息：",event.getBizNo(),event.getEventUniqueKey(),ex);
+                            throw new RuntimeException(ex);
+                        }
                     }
 
                 }
